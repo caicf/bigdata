bb5b00565ba3615596e302fdfd02445eda4eca70
cai_cf <cai_cf@126.com>
2016-06-14 17:01:42 +0800

    提一次提交

---
+++ src/main/java/com/bigdata/caicf/selenium/Sel.java
@@ -0,0 +1,55 @@
+package com.bigdata.caicf.selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Created by caicf on 2016/6/14.
+ */
+public class Sel {
+
+    public static void main(String[] args) {
+        //The path to the driver executable must be set by
+        //the webdriver.chrome.driver system property
+        System.setProperty("webdriver.chrome.driver","src\\main\\resources\\chromedriver.exe");
+
+        String path = "http://v.qq.com/cover/v/va107nb989aqmje.html?ptag=2345.movie";
+       // String path = "http://v.qq.com/prev/f/fetiylrqxyebh7g.html";
+        WebDriver driver = new ChromeDriver();
+        driver.get(path);
+
+        String breadcrumbXPath="/html/body/div[2]/div[1]/div[1]";
+
+        WebElement breadcrumb = driver.findElement(By.xpath(breadcrumbXPath));
+        WebElement subBreadcrumb1=breadcrumb.findElement(By.xpath(breadcrumbXPath+"/a[1]"));
+        WebElement subBreadcrumb2=breadcrumb.findElement(By.xpath(breadcrumbXPath+"/a[2]"));
+        WebElement subBreadcrumb3=breadcrumb.findElement(By.xpath(breadcrumbXPath+"/a[3]"));
+
+        String breadcrumbText=subBreadcrumb1.getText()+"/"+subBreadcrumb2.getText()+"/"+subBreadcrumb3.getText();
+
+        System.out.println(breadcrumbText);
+
+        String movieNameXPath="//*[@id=\"h1_title\"]";
+        String movieName=driver.findElement(By.xpath(movieNameXPath)).getText();
+
+        System.out.println(movieName);
+
+        String commentXPath="//*[@id=\"commentnum\"]";
+        String comment=driver.findElement(By.xpath(commentXPath)).getText();
+
+        System.out.println(comment);
+
+        String playtimesXPath="//*[@id=\"act_playnum\"]";
+        String playtimes=driver.findElement(By.xpath(playtimesXPath)).getText();
+        System.out.println(playtimes);
+
+
+
+
+
+
+        driver.quit();
+    }
+}
