c6947781f45a01420309a902b7f786bb398faea6
cai_cf <cai_cf@126.com>
2016-06-16 09:22:11 +0800

    可以存pageinfo和pagedata数据了

--- src/main/java/com/bigdata/caicf/TestDB.java
+++ src/main/java/com/bigdata/caicf/TestDB.java
@@ -5,8 +5,12 @@ import com.bigdata.caicf.dao.PageDataMapper;
 import com.bigdata.caicf.dao.PageInfoMapper;
 import com.bigdata.caicf.model.PageData;
 import com.bigdata.caicf.model.PageInfo;
+import com.bigdata.caicf.utility.WebUtil;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -26,6 +30,54 @@ public class TestDB {
 
     @Autowired
     private PageInfoMapper pageInfoMapper;
+
+    @Test
+    public void test2() {
+        WebDriver driver= WebUtil.getIEDriver();
+        String path="http://v.qq.com/cover/v/va107nb989aqmje.html?ptag=2345.movie";
+        new WebUtil().waitPageLoadComplete(driver);
+        driver.get(path);
+        //面包屑导航    电影>韩国>欲望
+        String breadcrumbXPath="/html/body/div[2]/div[1]/div[1]";
+        //电影名
+        String movieNameXPath="//*[@id=\"h1_title\"]";
+        //评论数量
+        String commentXPath="//*[@id=\"commentnum\"]";
+        //播放次数
+        String playtimesXPath="//*[@id=\"act_playnum\"]";
+
+        //标签
+        String tags="//*[@id=\"mod_descContent\"]/ul/li[1]/div";
+        //导演
+        String director="//*[@id=\"mod_descContent\"]/ul/li[2]/div";
+        //主演
+        String starring="//*[@id=\"mod_descContent\"]/ul/li[3]/div";
+        //简介
+        WebElement info=driver.findElement(By.xpath("//*[@id=\"btn_desc_expand\"]/span"));
+        info.click();
+        String infofull="//*[@id=\"mod_desc\"]/p[2]";
+
+        PageInfo pageInfo=new PageInfo();
+        pageInfo.setBreadcrumbtext(WebUtil.getWebElementText(By.xpath(breadcrumbXPath)));
+        pageInfo.setMoviename(WebUtil.getWebElementText(By.xpath(movieNameXPath)));
+        pageInfo.setComments(WebUtil.getWebElementText(By.xpath(commentXPath)));
+        pageInfo.setPlaytimes(WebUtil.getWebElementText(By.xpath(playtimesXPath)));
+        pageInfo.setTags(WebUtil.getWebElementText(By.xpath(tags)));
+        pageInfo.setDirector(WebUtil.getWebElementText(By.xpath(director)));
+        pageInfo.setStarring(WebUtil.getWebElementText(By.xpath(starring)));
+        pageInfo.setSummary(WebUtil.getWebElementText(By.xpath(infofull)));
+
+        pageInfoMapper.insert(pageInfo);
+        System.out.println(pageInfo.getId());
+        PageData pageData=new PageData();
+        pageData.setUrl(path);
+        pageData.setValue(JSON.toJSONString(pageInfo));
+        pageData.setDate(new Date());
+        pageData.setObjId(pageInfo.getId());
+        pageDataMapper.insert(pageData);
+
+        WebUtil.quitDriver();
+    }
 
     @Test
     public void test1() throws ClassNotFoundException, SQLException {
@@ -53,9 +105,6 @@ public class TestDB {
 //            System.out.println(          resultSet.getString(2)   );
 //            System.out.println(          resultSet.getString(3)   );
 //        }
-
-
-
     }
 
 }
